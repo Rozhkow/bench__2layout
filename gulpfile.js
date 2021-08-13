6be5c2d23a1e113bcee266e3993e50fcea575c21
Dzmitry Razhkou <Dzmitry.Razhkou@itechart-group.com>
2021-08-13 14:19:38 +0300

    feat: first screen was rewritten into scss + responsive on laptops and phones

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,79 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
-const sourcemaps = require("gulp-sourcemaps");
-const watch = require("gulp-watch");
-const autoprefixer = require("gulp-autoprefixer");
-//const postcss = require('gulp-postcss');
+const concat = require("gulp-concat");
+const imagemin = require("gulp-imagemin");
+const cache = require("gulp-cache");
 
-gulp.task("sass-compile", function () {
+const uglify = require("gulp-uglify-es").default;
+const minifyCSS = require("gulp-minify-css");
+
+const paths = {
+  src: {
+    js: "src/js/**/*.js",
+    sass: "src/scss/**/*.scss",
+    html: "src/main.html",
+    img: "src/assets/img/**/*.{jpg,jpeg,png,svg,gif}",
+    fonts: "src/assets/fonts/**/*.{ttf,otf}",
+  },
+  dist: {
+    js: "dist/",
+    sass: "dist/",
+    html: "dist/",
+    img: "dist/img",
+    fonts: "dist/fonts",
+  },
+};
+
+gulp.task("sass", () => {
   return gulp
-    .src("./scss/**/*.scss")
-    .pipe(sourcemaps.init())
+    .src(paths.src.sass)
     .pipe(sass().on("error", sass.logError))
-    .pipe(autoprefixer())
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("./css/"));
+    .pipe(minifyCSS())
+    .pipe(concat("main.min.css"))
+    .pipe(gulp.dest(paths.dist.sass));
 });
 
-gulp.task("watch", function () {
-  gulp.watch("./scss/**/*.scss", gulp.series("sass-compile"));
+gulp.task("js", () => {
+  return gulp
+    .src(paths.src.js)
+    .pipe(uglify())
+    .pipe(concat("main.min.js"))
+    .pipe(gulp.dest(paths.dist.js));
 });
+
+gulp.task("html", () => {
+  return gulp.src(paths.src.html).pipe(gulp.dest(paths.dist.html));
+});
+
+gulp.task("img", () => {
+  return gulp
+    .src(paths.src.img)
+    .pipe(
+      cache(
+        imagemin({
+          interlaced: true,
+          verbose: true,
+        })
+      )
+    )
+    .pipe(gulp.dest(paths.dist.img));
+});
+
+gulp.task("fonts", () => {
+  return gulp.src(paths.src.fonts).pipe(gulp.dest(paths.dist.fonts));
+});
+
+gulp.task(
+  "default",
+  gulp.series(["sass", "js", "html", "fonts", "img"], (cb) => {
+    gulp.watch(paths.src.sass, gulp.series("sass"));
+    gulp.watch(paths.src.js, gulp.series("js"));
+    gulp.watch(paths.src.html, gulp.series("html"));
+    gulp.watch(paths.src.img, gulp.series("img"));
+    gulp.watch(paths.src.fonts, gulp.series("fonts"));
+
+    cb();
+  })
+);
+
+gulp.task("clear cache", () => cache.clearAll());
